fe04233953e3052fa143d12a58373b88226b80e8
Syauqi <sauki084@gmail.com>
2025-05-01 11:44:36 +0700

    Serialize and Json format

--- filemanager/index.php
+++ filemanager/index.php
@@ -1,6 +1,54 @@
 <?php
 
-$name = "Muhammad Syauqi Murtadlo | Full Stack Developer";
-file_put_contents("name.txt", $name);
-$file = file_get_contents("name.txt");
-echo $file;
+// $name = "Muhammad Syauqi Murtadlo | Full Stack Developer";
+// file_put_contents("name.txt", $name);
+// $file = file_get_contents("name.txt");
+// echo $file;
+
+$anggota = [
+  [
+    "nama" => "Muhammad Syauqi Murtadlo",
+    "alamat" => "Jl. Imam Bonjol",
+    "no_hp" => "081234567890"
+  ],
+  [
+    "nama" => "Muhammad Syauqi Murtadlo",
+    "alamat" => "Jl. Imam Bonjol",
+    "no_hp" => "081234567890"
+  ],
+  [
+    "nama" => "Muhammad Syauqi Murtadlo",
+    "alamat" => "Jl. Imam Bonjol",
+    "no_hp" => "081234567890"
+  ]
+];
+
+$data = json_encode($anggota);
+file_put_contents("anggota.json", $data);
+$output = file_get_contents("anggota.json");
+$result = json_decode($output);
+print_r($result);
+
+// $anggota = [
+//   [
+//     "nama" => "Muhammad Syauqi Murtadlo",
+//     "alamat" => "Jl. Imam Bonjol",
+//     "no_hp" => "081234567890"
+//   ],
+//   [
+//     "nama" => "Muhammad Syauqi Murtadlo",
+//     "alamat" => "Jl. Imam Bonjol",
+//     "no_hp" => "081234567890"
+//   ],
+//   [
+//     "nama" => "Muhammad Syauqi Murtadlo",
+//     "alamat" => "Jl. Imam Bonjol",
+//     "no_hp" => "081234567890"
+//   ]
+// ];
+
+// $data = serialize($anggota);
+// file_put_contents("anggota.txt", $data);
+// $output = file_get_contents("anggota.txt");
+// $result = unserialize($output);
+// print_r($result);

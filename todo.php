78a550b10110ac916445e111d767c70056c40fdd
Syauqi <sauki084@gmail.com>
2025-05-04 00:38:36 +0700

    Todo app - create form

---
+++ todo.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>Todo App</h1>
+  <form action="" method="">
+    <label for="todo">Apa Kegiatanmu hari ini?</label> <br>
+    <input type="text" name="todo" required>
+    <button type="submit">Add</button>
+  </form>
+  <ul>
+    <li>
+      <input type="checkbox" name="todo" id="todo">
+      <label for="todo">Belajar Bahasa Pemrograman PHP</label>
+      <button type="submit">Delete</button>
+    </li>
+    <li>
+      <input type="checkbox" name="todo" id="todo">
+      <label for="todo">Belajar Framework Laravel</label>
+      <button type="submit">Delete</button>
+    </li>
+    <li>
+      <input type="checkbox" name="todo" id="todo">
+      <label for="todo">Belajar Database MySQL</label>
+      <button type="submit">Delete</button>
+    </li>
+    <li>
+      <input type="checkbox" name="todo" id="todo">
+      <label for="todo">Belajar Docker</label>
+      <button type="submit">Delete</button>
+    </li>
+  </ul>
+</body>
+
+</html>
